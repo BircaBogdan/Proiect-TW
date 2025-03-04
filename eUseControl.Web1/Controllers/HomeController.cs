22cb19a40155cbefb54cd011f05bb5123f5b9047
Maxim Turcan <maxim.turcan@isa.utm.md>
2025-03-04 18:15:01 +0200

    Implementarea Servciilor cu ajutorul modelului Domains.cs

--- eUseControl.Web1/Controllers/HomeController.cs
+++ eUseControl.Web1/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
+using eUseControl.Web1.Models;
 
 
 namespace eUseControl.Web1.Controllers
@@ -13,7 +14,15 @@ namespace eUseControl.Web1.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+               Domains s = new Domains();
+               s.Title = "Servicii";
+               s.Description = "Serviciile medicale oferite";
+               s.Services = new List<string> { "Cardiologie", "Starea Pulmonară","Neurologie","Pediatrie","Stomatologie","Laborator" };
+               s.Images = new List<string> { "fa fa-heartbeat text-primary fs-4", "fa fa-x-ray text-primary fs-4", "fa fa-brain text-primary fs-4", "fa fa-wheelchair text-primary fs-4", "fa fa-tooth text-primary fs-4", "fa fa-vials text-primary fs-4" };
+
+
+            return View(s);
         }
+
     }
 }
---
+++ eUseControl.Web1/models/Domains.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eUseControl.Web1.Models
+{
+     public class Domains
+     {
+          public string Title { get; set; }
+          public string Description { get; set; }
+          public List<string> Services { get; set; }
+          public List<string> Images { get; set; }
+     }
+}
