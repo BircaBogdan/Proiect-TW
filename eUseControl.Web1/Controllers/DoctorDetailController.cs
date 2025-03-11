a522e7f44f63f991a69327bde740500f4f0c27bd
Bogdan Birca <bogdan.birca@isa.utm.md>
2025-03-11 18:29:21 +0200

    Clasa/Modelul Doctor

---
+++ eUseControl.Web1/Controllers/DoctorDetailController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eUseControl.BusinessLogic.Interfaces;
+
+namespace eUseControl.Web1.Controllers
+{
+    public class DoctorDetailController : Controller
+    {
+        private IDoctor _doctor;
+        // GET: DoctorDetail
+        DoctorDetailController()
+        {
+            BusinessLogic business1 = new BusinessLogic();
+
+            _doctor = business1.GetDoctorBL();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetDoctor(int id)
+        {
+
+            DoctorDetail = _doctor.GetDetailDoctor(id);
+            return View();
+        
+    }
+    }
+}
